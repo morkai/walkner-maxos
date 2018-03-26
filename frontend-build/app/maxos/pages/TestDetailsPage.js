b2408ee831a8782393652298de4433e444e34d77
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-03-26 16:04:15 +0200

    Updated the build.

--- frontend-build/app/maxos/pages/TestDetailsPage.js
+++ frontend-build/app/maxos/pages/TestDetailsPage.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-maxos> licensed under <CC BY-NC-SA 4.0>
 
-define(["app/i18n","app/user","app/core/pages/DetailsPage","../controller","../views/TestDetailsView"],function(e,t,s,a,i){"use strict";return s.extend({DetailsView:i,actions:function(){var t=[];return"success"===this.model.get("status")&&a.auth.printLabels()&&t.push({icon:"print",label:e("maxos","PAGE_ACTIONS:printLabels"),callback:function(){}}),t}})});
+define(["app/i18n","app/user","app/core/pages/DetailsPage","../controller","../views/TestDetailsView"],function(e,t,i,n,a){"use strict";return i.extend({DetailsView:a,actions:function(){var e=[];return e}})});
